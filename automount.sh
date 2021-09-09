71e756f85e19880a1483ca312adcbd6c5a9ae459
Doridian <git@doridian.net>
2021-09-09 03:57:13 +0000

    Automount based on UUID

--- automount.sh
+++ automount.sh
@@ -2,12 +2,36 @@
 
 set -e
 
+BCACHE_DEVICES="$(lsblk --fs --list --json | jq -r '.blockdevices[] | select(.fstype == "bcache") | .name')"
+
+bcachefs_uuid() {
+	DEV="$1"
+	bcachefs show-super "$DEV" | grep -F 'External UUID' | cut -d: -f2 | tr -d '\r\n\t '
+}
+
 bcachefs_mount() {
-	LABEL="$1"
+	UUID="$1"
 	MOUNTPOINT="$2"
-	bcachefs unlock "/dev/disk/by-partlabel/$LABEL" < /mnt/keydisk/bcachefs_keyfile
-	DEVICES="$(blkid -t PARTLABEL="$LABEL" -o device | tr '\n' ':' | sed -z '$ s/:$//')"
+	KEYFILE="$3"
+
+	DEVICE=""
+	DEVICES=""
+	for dev in $BCACHE_DEVICES
+	do
+		dev="/dev/$dev"
+		if [ "$(bcachefs_uuid "$dev")" == "$UUID" ]
+		then
+			DEVICE="$dev"
+			DEVICES="$DEVICES$dev:"
+		fi
+	done
+
+	DEVICES="$(echo -n "$DEVICES" | sed -z '$ s/:$//')"
+
+	if [ ! -z "$KEYFILE" ]
+	then
+		bcachefs unlock "$DEVICE" < "$KEYFILE"
+	fi
 	mount -t bcachefs "$DEVICES" "$MOUNTPOINT"
 }
 
-bcachefs_mount bcachefs_backup /mnt/migratedrv
